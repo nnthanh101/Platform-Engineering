205428450778696b540f90a270d23447b6781c14
Thanh Nguyen <nnthanh101@gmail.com>
2021-04-19 10:06:52 +0700

    🚀 https://devsecops.job4u.io/en/eks/terraform-101/

--- terraform/stacks/tf-vpc/aws.tf
+++ terraform/stacks/tf-vpc/aws.tf
@@ -1,16 +1,27 @@
 terraform {
   required_version = "~> 0.14.10"
   required_providers {
-    aws = {
-      source = "hashicorp/aws"
+    aws      = {
+      source   = "hashicorp/aws"
       ##  Allow any 3.36+  version of the AWS provider
-      version = "~> 3.36"
+      version  = "~> 3.36"
+    }
+    null     = {
+      source   = "hashicorp/null"
+      version  = "~> 3.1"
+    }
+    external = {
+      source   = "hashicorp/external"
+      version  = "~> 2.1"
     }
   }
 }
 
 provider "aws" {
-  region                  = "ap-southeast-1"
+  region                  = var.region
   shared_credentials_file = "~/.aws/credentials"
-  profile                 = "default"
+  profile                 = var.profile
 }
+
+provider "null"     {}
+provider "external" {}
